5548d955e5cd07e9b10d8d8f90e43aa90e4da2be
Frank Lanke Fu Tarimo <fulkast@gmail.com>
2015-06-11 18:46:18 +0200

    the clean up

--- Frank/Programming/Exercise 1/stdize.m
+++
@@ -1,4 +0,0 @@
-function Y = stdize(X)
-Y = (X - repmat(mean(X),[length(X) 1])) ./ repmat(std(X),[length(X) 1]);
-end
-
--- Frank/Programming/Exercise 1/stdize_reference_vec.m
+++
@@ -1,7 +0,0 @@
-function Y = stdize_reference_vec(X)
-
-m = mean(X);
-s = std(X);
-Y = (X - repmat(m,size(X,1),1)) ./ repmat(s,size(X,1),1);
-end
-
