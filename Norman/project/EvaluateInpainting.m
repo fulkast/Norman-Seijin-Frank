382902f0da55e34f9d428cda59a5c5737f509362
normanius <normanius@gmail.com>
2015-06-13 19:39:10 +0200

    Refactored evaluation script.

--- Norman/project/EvaluateInpainting.m
+++ Norman/project/EvaluateInpainting.m
@@ -1,78 +1,90 @@
-% Measure approximation error and compression ratio for several images.
+%%
+% Input
 %
-% NOTE Images must be have .png ending and reside in the same folder.
+% maskType:
+%   0: from file
+%   1: salt and pepper noise
+% maskPepperness
+%   value between 0.0 or 1.0
+
 dataDir = 'data';
-file_list = dir(dataDir); 
-k = 1;
-siz=512;
-Errors = []; % mean squared errors for each image would be stored here
+maskType = 0;
+maskPepperness = 0.7;
+showResults = true;
 
+outDir = 'output';
+saveResults = true;
 
-for i = 3:length(file_list) % running through the folder
+%% Setup
+fileList = dir(dataDir); 
+
+% Mean squared errors.
+errors = []; 
+count = 1;
+
+%% Loop over data dir
+for i = 3:length(fileList) 
     
-    file_name = fullfile(dataDir, file_list(i).name); % get current filename
-    
-   % file_name = file_list(i).name; % get current filename
-    
-    % Only keep the images in the loop
-    if ( (file_name(end-7:end) == 'mask.png'))
+    % Input image and mask.
+    fileName = fileList(i).name;
+    filePath = fullfile(dataDir, fileName) 
+    if (strcmp(filePath(end-7:end), 'mask.png'))
         continue;
     end
-   mask_name = [file_name(1:end-4) '_mask.png'];
-    % mask_name = ['mask.png'];
+    maskName = [filePath(1:end-4) '_mask.png'];
         
-    % Read image, convert to double precision and map to [0,1] interval
-    fprintf('Reading file %s...\n', file_name)
-   try
-    I = imread(file_name); 
-   catch
-       continue;
-   end
-    I = double(I) / 255; 
-    [a,b,c]=size(I);
-    if (ceil(siz/min(a,b))>1)
-        I=expand(I,ceil(siz/min(a,b)));
-    elseif (floor(min(a,b)/siz)>1)
-        I=compress(I,floor(min(a,b)/siz));
+    % Read image.
+    fprintf('Reading file %s...\n', filePath)
+    try
+        I = imread(filePath);
+        if size(I,3) > 1
+            I = rgb2gray(I);
+        end
+    catch
+        continue;
     end
-    n1=floor(size(I,1)/2);
-    n2=floor(size(I,2)/2);
-    I=I(n1-siz/2+1:n1+siz/2,n2-siz/2+1:n2+siz/2,1);
-    % Read the respective binary mask
-    % EVALUATION IS DONE WITH A FIXED MASK
-    mask=ones(size(I));
-    mask = imread(mask_name);
-    mask=mask;
-   
     
-    %pepper noise
-    %mask=ones(size(I));
-    for i=1:size(mask,1)*size(mask,2)/2*0
-        a=floor(rand(1)*size(mask,1)+1);
-        b=floor(rand(1)*size(mask,2)+1);
-        mask(a,b)=0;
+    % Convert to double precision and map to [0,1] interval.
+    I = double(I) / 255; 
+    
+    % Read mask.
+    switch maskType
+        case 0
+            mask = imread(maskName);
+        case 1
+            mask = random_mask(size(I), maskPepperness);
     end
-
- mask=mask(:,:,1);
+    mask=im2double(mask);
+    
+    % Apply mask.
     I_mask = I;
     I_mask(~mask) = 0;
-mask=im2double(mask);
-%imshow(I_mask)
-    % Call the main inPainting function
-    tic
-   I_rec = inPainting(I_mask, mask);
-   toc
-   figure
-   imshow(I_rec);
-   imwrite(I_rec, 'out.png');
-   pause;
-    % Measure approximation error
-    Errors(k) = mean(mean(mean( ((I - I_rec) ).^2)));
-    fprintf('error= %s  \n',Errors(k));
-    k = k+1;
-   % break;
+    
+    % In-painting.
+    I_rec = inPainting(I_mask, mask);
+   
+    if showResults
+        figure(1)
+        imshow(I_rec);
+        figure(2)
+        imshow(I_mask);
+        pause
+    end
+    
+    if saveResults
+        if ~exist(outDir, 'dir')
+            mkdir(outDir);
+        end
+        outFile = fullfile(outDir, fileName);
+        fprintf('Saving file %s...\n', outFile); 
+        imwrite(I_rec, outFile);
+    end
+    
+    % Measure approximation error and print result.
+    errors(count) = mean(mean(mean(((I - I_rec) ).^2)));
+    fprintf('error= %s  \n',errors(count));
+    count = count + 1;
 end
 
-Result(1) = mean(Errors);
-
-disp(['Average quadratic error: ' num2str(Result(1))])
+result(1) = mean(errors);
+disp(['Average quadratic error: ' num2str(result(1))])
